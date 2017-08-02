ad20a083a5550a4ce7caf4584994c842871b7900
Moshabbar Hussain <talktomoshabbar@gmail.com>
2017-08-02 21:48:22 +0530

    Get result by searching Roll No

---
+++ search_by_Roll_No.py
@@ -0,0 +1,275 @@
+print "........WELCOME TO RESULT PORTAL........."
+import time
+name={
+   
+  
+   "Moshabbar": [
+      "...........Moshabbar::Marksheet...............",
+      "Roll No-::AS6789",
+      "PHYSICS MARK-----::78",
+      "CHEMISTRY MARK---::90",
+      "MATH MARK--------::78",
+      "BIOLOGY MARK-----::67",
+      "ENGLISH MARK-----::71",
+      "HINDI MARK-------::89",
+      "...................................",
+      "Total------------::473",
+      "Percentage::78.83%",
+      "Result:Passed with First Class"
+   ],
+   
+
+   "Rahul": [
+      "...........Rahul::Marksheet...............",
+      "Roll No-::AS8780",
+      "PHYSICS MARK-----::71",
+      "CHEMISTRY MARK---::80",
+      "MATH MARK--------::78",
+      "BIOLOGY MARK-----::67",
+      "ENGLISH MARK-----::71",
+      "HINDI MARK-------::50",
+      "...................................",
+      "Total------------::417",
+      "Percentage::69.5%",
+      "Result:Passed with First Class"
+   ],
+
+   "Ashu": [
+      "...........Ashu::Marksheet...............",
+      "Roll No-::AS9719",
+      "PHYSICS MARK----::89",
+      "CHEMISTRY MARK--::90",
+      "MATH MARK-------::88",
+      "BIOLOGY MARK----::77",
+      "ENGLISH MARK----::71",
+      "HINDI MARK------::99",
+      "...................................",
+      "Total-----------::514",
+      "Percentage::85.66%",
+      "Result:Passed with First Class with Distinction"
+   ],
+   
+   "Ameer": [
+      "...........Ameer::Marksheet...............",
+      "Roll No-::AS2780",
+      "PHYSICS MARK-----::89",
+      "CHEMISTRY MARK---::90",
+      "MATH MARK--------::88",
+      "BIOLOGY MARK-----::77",
+      "ENGLISH MARK-----::71",
+      "HINDI MARK-------::99",
+      "...................................",
+      "Total------------::514",
+      "Percentage::85.66%",
+      "Result:Passed with First Class with Distinction"
+   ],
+   "Irshad": [
+      "...........Irshad::Marksheet...............",
+      "Roll No-::AS3781",
+      "PHYSICS MARK------::79",
+      "CHEMISTRY MARK----::55",
+      "MATH MARK---------::67",
+      "BIOLOGY MARK------::77",
+      "ENGLISH MARK------::71",
+      "HINDI MARK--------::67",
+      "...................................",
+      "Total-------------::416",
+      "Percentage::69.33%",
+      "Result:Passed with First Class"
+   ],
+   "Resmi": [
+      "...........Resmi::Marksheet...............",
+      "Roll No-::AS4780",
+      "PHYSICS MARK------::35",
+      "CHEMISTRY MARK----::40",
+      "MATH MARK---------::38",
+      "BIOLOGY MARK------::47",
+      "ENGLISH MARK------::31",
+      "HINDI MARK--------::59",
+      "...................................",
+      "Total-------------::250",
+      "Percentage::41.66%",
+      "Result:Passed with Third Class"
+   ],
+   "Haidar": [
+      "...........Haider::Marksheet...............",
+      "Roll No-::AS6181"
+      "PHYSICS MARK------::49",
+      "CHEMISTRY MARK----::50",
+      "MATH MARK---------::68",
+      "BIOLOGY MARK------::37",
+      "ENGLISH MARK------::51",
+      "HINDI MARK--------::59",
+      "...................................",
+      "Total-------------::314",
+      "Percentage::52.33%",
+      "Result:Passed with Second Class"
+   ]
+    
+}
+## If you want to get data from database and want Result just connect with database and .....coding like below...
+Roll_No=raw_input("Enter Roll No:")
+if(Roll_No=="AS6789" or Roll_No=="AS8780" or Roll_No=="AS9719" or Roll_No=="AS2780"
+   or Roll_No=="AS3781" or Roll_No=="AS4780" or Roll_No=="AS6181"):
+
+    if(Roll_No=="AS6789"):
+        print("Please Wait Your Result is searching.......")
+        time.sleep(5)
+        print "Name::MOSHABBAR HUSSAIN"
+        print name["Moshabbar"][1]
+        print name["Moshabbar"][0]
+        print name["Moshabbar"][2]
+        print name["Moshabbar"][3]
+        print name["Moshabbar"][4]
+        print name["Moshabbar"][5]
+        print name["Moshabbar"][6]
+        print name["Moshabbar"][7]
+        print name["Moshabbar"][8]
+        print name["Moshabbar"][9]
+        print name["Moshabbar"][10]
+        print name["Moshabbar"][11]
+        #print name["Moshabbar"]
+        #print "Please Wait Result is Searching......"
+        #time.sleep(10)
+        #for result in name["Moshabbar"]:
+            #print result
+
+    elif(Roll_No=="AS8780"):
+        print("Please Wait Your Result is searching.......")
+        time.sleep(5)
+        print "Name::RAHUL AMEEN"
+        print name["Rahul"][1]
+        print name["Rahul"][0]
+        print name["Rahul"][2]
+        print name["Rahul"][3]
+        print name["Rahul"][4]
+        print name["Rahul"][5]
+        print name["Rahul"][6]
+        print name["Rahul"][7]
+        print name["Rahul"][8]
+        print name["Rahul"][9]
+        print name["Rahul"][10]
+        print name["Rahul"][11]
+        
+        #print name["Rahul"]
+        #print "Please Wait Result is Searching......"
+        #time.sleep(10)
+        #for result in name["Rahul"]:
+            #print result
+
+    elif(Roll_No=="AS9719"):
+        print("Please Wait Your Result is searching.......")
+        time.sleep(5)
+        print "Name::ASHU JHARKAND"
+        print name["Ashu"][1]
+        print name["Ashu"][0]
+        print name["Ashu"][2]
+        print name["Ashu"][3]
+        print name["Ashu"][4]
+        print name["Ashu"][5]
+        print name["Ashu"][6]
+        print name["Ashu"][7]
+        print name["Ashu"][8]
+        print name["Ashu"][9]
+        print name["Ashu"][10]
+        print name["Ashu"][11]
+        
+        #print name["Ashu"]
+        #print "Please Wait Result is Searching......"
+        #time.sleep(10)
+        #for result in name["Ashu"]:
+            #print result
+
+
+    elif(Roll_No=="AS2780"):
+        print("Please Wait Your Result is searching.......")
+        time.sleep(5)
+        print "Name::AMEER HUSSAIN"
+        print name["Ameer"][1]
+        print name["Ameer"][0]
+        print name["Ameer"][2]
+        print name["Ameer"][3]
+        print name["Ameer"][4]
+        print name["Ameer"][5]
+        print name["Ameer"][6]
+        print name["Ameer"][7]
+        print name["Ameer"][8]
+        print name["Ameer"][9]
+        print name["Ameer"][10]
+        print name["Ameer"][11]
+        #print name["Ashu"]
+        #print "Please Wait Result is Searching......"
+        #time.sleep(10)
+        #for result in name["Ameer"]:
+            #print result
+
+    elif(Roll_No=="AS3781"):
+        print("Please Wait Your Result is searching.......")
+        time.sleep(5)
+        print "Name::IRSHAD KHAN"
+        print name["Irshad"][1]
+        print name["Irshad"][0]
+        print name["Irshad"][2]
+        print name["Irshad"][3]
+        print name["Irshad"][4]
+        print name["Irshad"][5]
+        print name["Irshad"][6]
+        print name["Irshad"][7]
+        print name["Irshad"][8]
+        print name["Irshad"][9]
+        print name["Irshad"][10]
+        print name["Irshad"][11]
+        #print name["Ashu"]
+        #print "Please Wait Result is Searching......"
+        #time.sleep(10)
+        #for result in name["Irshad"]:
+            #print result
+
+    elif(Roll_No=="AS4780"):
+        print("Please Wait Your Result is searching.......")
+        time.sleep(5)
+        print "Name::RESMI KHATUN"
+        print name["Resmi"][1]
+        print name["Resmi"][0]
+        print name["Resmi"][2]
+        print name["Resmi"][3]
+        print name["Resmi"][4]
+        print name["Resmi"][5]
+        print name["Resmi"][6]
+        print name["Resmi"][7]
+        print name["Resmi"][8]
+        print name["Resmi"][9]
+        print name["Resmi"][10]
+        print name["Resmi"][11]
+        #print name["Ashu"]
+        #print "Please Wait Result is Searching......"
+        #time.sleep(10)
+        #for result in name["Haider"]:
+            #print result
+            
+    elif(Roll_No=="AS6181"):
+        print("Please Wait Your Result is searching.......")
+        time.sleep(5)
+        print "Name::HAIDER ALI"
+        print name["Haidar"][1]
+        print name["Haidar"][0]
+        print name["Haidar"][2]
+        print name["Haidar"][3]
+        print name["Haidar"][4]
+        print name["Haidar"][5]
+        print name["Haidar"][6]
+        print name["Haidar"][7]
+        print name["Haidar"][8]
+        print name["Haidar"][9]
+        print name["Haidar"][10]
+        print name["Haidar"][11]
+        #print name["Ashu"]
+        #print "Please Wait Result is Searching......"
+        #time.sleep(10)
+        #for result in name["Resmi"]:
+            #print result        
+            
+
+            
+else:
+    print "please Enter Correct Roll No "
